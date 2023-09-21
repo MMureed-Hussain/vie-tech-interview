fa833aa3bdaff45bae2c5fbb4d1204f5c9b41b53
MMureed-Hussain <malikmureedh@gmail.com>
2023-09-21 13:10:01 +0500

    First commit

--- src/components/Navbars.jsx
+++ src/components/Navbars.jsx
@@ -1,88 +1,82 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export const Navbars = () => {
   return (
-    <>
-      <Navbar className="bg-light" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand to="/" className="text-dark">
-            LOGO
-          </Navbar.Brand>
-          <Nav className="me-auto" style={{ fontWeight: "bold" }}>
-            <Link
-              className="text-success mx-3"
-              to="/"
-              style={{ textDecoration: "none" }}
-            >
-              <span>
-                <i className="fa fa-home mx-1"></i>
-              </span>
-              HOME
-            </Link>
-
-            <Link
-              to="/about"
-              className="text-secondary mx-3"
-              style={{ textDecoration: "none" }}
-            >
-              <span>
-                <i className="fa fa-home mx-1"></i>
-              </span>
-              ABOUT
-            </Link>
-            <Link
-              to="/class"
-              className="text-warning mx-3"
-              style={{ textDecoration: "none" }}
-            >
-              <span>
-                <i className="fa fa-home mx-1"></i>
-              </span>
-              CLASSES
-            </Link>
-            <Link
-              to="/blog"
-              className="text-danger mx-3"
-              style={{ textDecoration: "none" }}
-            >
-              <span>
-                <i className="fa fa-home mx-1"></i>
-              </span>
-              BLOG
-            </Link>
-            <Link
-              to="/pages"
-              className="text-info mx-3"
-              style={{ textDecoration: "none" }}
-            >
-              <span>
-                <i className="fa fa-home mx-1"></i>
-              </span>
-              PAGES
-            </Link>
-            <Link
-              className="text-primary mx-3"
-              to="/contact"
-              style={{ textDecoration: "none" }}
-            >
-              <span>
-                <i className="fa fa-home mx-1"></i>
-              </span>
-              CONTACT
-            </Link>
-            <Link to="" className="text-info me-4">
-              <i className="fa fa-search"></i>
-            </Link>
-            <Link to="" className="text-info me-4">
-              <i className="fa fa-shopping-cart"></i>
-            </Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    </>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav" style={{justifyContent: "center"}}>
+        <Nav className="mr-auto" style={{ fontWeight: "bold" }}>
+          <Link
+            className="text-success mx-3"
+            to="/"
+            style={{ textDecoration: "none" }}
+          >
+            <span>
+              <i className="fa fa-home mx-1"></i>
+            </span>
+            HOME
+          </Link>
+          <Link
+            to="/about"
+            className="text-secondary mx-3"
+            style={{ textDecoration: "none" }}
+          >
+            <span>
+              <i className="fa fa-home mx-1"></i>
+            </span>
+            ABOUT
+          </Link>
+          <Link
+            to="/class"
+            className="text-warning mx-3"
+            style={{ textDecoration: "none" }}
+          >
+            <span>
+              <i className="fa fa-home mx-1"></i>
+            </span>
+            CLASSES
+          </Link>
+          <Link
+            to="/blog"
+            className="text-danger mx-3"
+            style={{ textDecoration: "none" }}
+          >
+            <span>
+              <i className="fa fa-home mx-1"></i>
+            </span>
+            BLOG
+          </Link>
+          <Link
+            to="/pages"
+            className="text-info mx-3"
+            style={{ textDecoration: "none" }}
+          >
+            <span>
+              <i className="fa fa-home mx-1"></i>
+            </span>
+            PAGES
+          </Link>
+          <Link
+            className="text-primary mx-3"
+            to="/contact"
+            style={{ textDecoration: "none" }}
+          >
+            <span>
+              <i className="fa fa-home mx-1"></i>
+            </span>
+            CONTACT
+          </Link>
+          <Link to="" className="text-info mx-3">
+            <i className="fa fa-search"></i>
+          </Link>
+          <Link to="" className="text-info mx-3">
+            <i className="fa fa-shopping-cart"></i>
+          </Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
