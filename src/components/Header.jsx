775fa63cfb4589376b899a2dc123c984c5a3ac92
MMureed-Hussain <malikmureedh@gmail.com>
2023-09-21 16:16:41 +0500

    First commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,65 @@
+// import React from "react";
+// import Container from "react-bootstrap/Container";
+// import Nav from "react-bootstrap/Nav";
+// import Navbar from "react-bootstrap/Navbar";
+// import { Link } from "react-router-dom";
+
+// export const Header = () => {
+//   return (
+//     <>
+//       <Navbar data-bs-theme="dark" style={{ backgroundColor: "#9f1094" }}>
+//         <Container>
+//           <Nav
+//             className="me-auto"
+//             style={{
+//               display: "flex",
+//               justifyContent: "space-between",
+//               width: "100%",
+//             }}
+//           >
+//             <div className="me-5">
+//               <span className="text-light">
+//                 <i class="fa fa-clock-o m-1"></i> 9:30am-6-30pm Mon-Sun |
+//                 <i class="fa fa-phone m-1 "></i>+9230001234567 |
+//                 <i class="fa-solid fa-location-dot mx-1"></i>DHA Lahore
+//               </span>
+//             </div>
+//             <div className="" style={{marginRight: "120px"}}>
+//               <Link to="" className="text-white me-4">
+//                 <i class="fab fa-facebook-messenger "></i>
+//               </Link>
+//               <Link to="" className="text-white me-4">
+//                 <i className="fab fa-twitter"></i>
+//               </Link>
+//               <Link to="" className="text-white me-4">
+//               <i class="fa-brands fa-vimeo-v"></i>
+//               </Link>
+//               <Link to="" className="text-white me-4">
+//                 <i className="fab fa-skype"></i>
+//               </Link>
+//               <Link to="" className="text-white me-4">
+//               <i class="fa-solid fa-wifi"></i>
+//               </Link>
+//             </div>
+//           </Nav>
+//         </Container>
+//       </Navbar>
+//     </>
+//   );
+// };
+
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
     <>
-      <Navbar data-bs-theme="dark" style={{ backgroundColor: "#9f1094" }}>
+      <Navbar
+        data-bs-theme="dark"
+        style={{ backgroundColor: "#9f1094" }}
+        expand="lg"
+      >
         <Container>
           <Nav
             className="me-auto"
@@ -17,28 +69,38 @@ export const Header = () => {
               width: "100%",
             }}
           >
-            <div className="me-5">
-              <span className="text-light">
-                <i class="fa fa-clock-o m-1"></i> 9:30am-6-30pm Mon-Sun |
-                <i class="fa fa-phone m-1 "></i>+9230001234567 |
-                <i class="fa-solid fa-location-dot mx-1"></i>DHA Lahore
-              </span>
-            </div>
-            <div className="">
-              <Link to="" className="text-white me-4">
-                <i class="fab fa-facebook-messenger "></i>
+            <Nav className="me-5 ">
+              <Link className="text-light" style={{textDecoration: "none"}}>
+                <i
+                  className="fa fa-clock-o mx-2"
+                  style={{ textDecoration: "none" }}
+                ></i>
+                9:30am-6-30pm Mon-Sun |
               </Link>
-              <Link to="" className="text-white me-4">
+              <Link className="text-light" style={{textDecoration: "none"}}>
+                <i className="fa fa-phone mx-2"></i>
+                +9230001234567 |
+              </Link>
+              <Link className="text-light" style={{textDecoration: "none"}}>
+                <i className="fa-solid fa-location-dot mx-2"></i>
+                DHA Lahore
+              </Link>
+            </Nav>
+            <div className=""style={{marginRight: "120px"}}>
+              <Link to="" className="text-white me-3">
+                <i className="fab fa-facebook-messenger "></i>
+              </Link>
+              <Link to="" className="text-white me-3">
                 <i className="fab fa-twitter"></i>
               </Link>
-              <Link to="" className="text-white me-4">
-              <i class="fa-brands fa-vimeo-v"></i>
+              <Link to="" className="text-white me-3">
+                <i className="fa-brands fa-vimeo-v"></i>
               </Link>
-              <Link to="" className="text-white me-4">
+              <Link to="" className="text-white me-3">
                 <i className="fab fa-skype"></i>
               </Link>
-              <Link to="" className="text-white me-4">
-              <i class="fa-solid fa-wifi"></i>
+              <Link to="" className="text-white me-3">
+                <i className="fa-solid fa-wifi"></i>
               </Link>
             </div>
           </Nav>
