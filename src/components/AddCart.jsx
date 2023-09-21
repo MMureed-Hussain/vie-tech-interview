f15ae310788405add34a2b5051ae90dfae230ccd
MMureed-Hussain <malikmureedh@gmail.com>
2023-09-21 17:54:32 +0500

    Add to Cart Sidebar modal is open when click on Icon

--- src/components/AddCart.jsx
+++ src/components/AddCart.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
 
-export const AddCart = () => {
+export const AddCart = ({ show, handleClose }) => {
   return (
-    <div>AddCart</div>
-  )
-}
+    <>
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          right: 0,
+          height: "100%",
+          width: "500px",
+          backgroundColor: "#f8f9fa",
+          boxShadow: "-1px 0 10px rgba(0,0,0,0.1)",
+          transition: "0.3s",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <div className="p-3" style={{ flex: "1" }}>
+          <Modal.Header>
+            <Modal.Title variant="secondary">Sidebar Content</Modal.Title>
+            <i
+              className="fa fa-close"
+              style={{ cursor: "pointer" }}
+              onClick={handleClose}
+            ></i>
+          </Modal.Header>
+          <Modal.Body>Are you sure you want to Add to Cart</Modal.Body>
+        </div>
+        <Button
+          className="bg-info text-light m-3"
+          onClick={handleClose}
+          style={{ padding: "10px", textAlign: "center" }}
+        >
+          Close
+        </Button>
+      </div>
+    </>
+  );
+};
--- src/components/Navbars.jsx
+++ src/components/Navbars.jsx
@@ -1,99 +1,118 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav} from "react-bootstrap";
 import { Link } from "react-router-dom";
-// import logoImage from '/images/logo192.png'
 import logoImage from "../images/logo192.png";
+import { AddCart } from "./AddCart";
 
 export const Navbars = () => {
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const handleTrashIconClick = () => {
+    setShowSidebar(true);
+  };
+
+  const handleCloseSidebar = () => {
+    setShowSidebar(false);
+  };
+
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Toggle className="m-2" aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse
-        id="basic-navbar-nav"
-        style={{ justifyContent: "center" }}
-      >
-        <Nav className="mr-auto" style={{ fontWeight: "bold" }}>
-          <Link className="mx-3" to="/" style={{ textDecoration: "none" }}>
-            <img
-              src={logoImage}
-              alt="Logo"
-              style={{ maxWidth: "100px", maxHeight: "30px" }} // Adjust image dimensions as needed
-            />
-            <span style={{ color: "green" }}>K</span>
-            <span style={{ color: "blue" }}>i</span>
-            <span style={{ color: "red" }}>tt</span>
-            <span style={{ color: "blue" }}>o</span>
-            <span style={{ color: "red" }}>n</span>
-          </Link>
-          <Link
-            className="text-success mx-3"
-            to="/"
-            style={{ textDecoration: "none" }}
-          >
-            <span>
-              <i className="fa fa-home mx-1"></i>
-            </span>
-            HOME
-          </Link>
-          <Link
-            to="/about"
-            className="mx-3"
-            style={{ textDecoration: "none", color:"#9f1094" }}
-          >
-            <span>
-              <i className="fa fa-user mx-1"></i>
-            </span>
-            ABOUT
-          </Link>
-          <Link
-            to="/class"
-            className="text-danger mx-3"
-            style={{ textDecoration: "none" }}
-          >
-            <span>
-              <i className="fa fa-file-text-o mx-1"></i>
-            </span>
-            CLASSES
-          </Link>
-          <Link
-            to="/blog"
-            className="text-info mx-3"
-            style={{ textDecoration: "none" }}
-          >
-            <span>
-              <i className="fa fa-pencil mx-1"></i>
-            </span>
-            BLOG
-          </Link>
-          <Link
-            to="/pages"
-            className="mx-3"
-            style={{ textDecoration: "none", color: "red"}}
-          >
-            <span>
-              <i className="fa fa-align-justify mx-1"></i>
-            </span>
-            PAGES
-          </Link>
-          <Link
-            className="text-primary mx-3"
-            to="/contact"
-            style={{ textDecoration: "none" }}
-          >
-            <span>
-              <i class="fa-solid fa-location-dot mx-1"></i>
-            </span>
-            CONTACT
-          </Link>
-          <Link to="" className="mx-3" style={{color:"#9f1094"}}>
-            <i className="fa fa-search"></i>
-          </Link>
-          <Link to="" className="text-secondary mx-3">
-            <i className="fa fa-trash-o"></i>
-          </Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Toggle className="m-2" aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          style={{ justifyContent: "center" }}
+        >
+          <Nav className="mr-auto" style={{ fontWeight: "bold" }}>
+            <Link className="mx-3" to="/" style={{ textDecoration: "none" }}>
+              <img
+                src={logoImage}
+                alt="Logo"
+                style={{ maxWidth: "100px", maxHeight: "30px" }} // Adjust image dimensions as needed
+              />
+              <span style={{ color: "green" }}>K</span>
+              <span style={{ color: "blue" }}>i</span>
+              <span style={{ color: "red" }}>tt</span>
+              <span style={{ color: "blue" }}>o</span>
+              <span style={{ color: "red" }}>n</span>
+            </Link>
+            <Link
+              className="text-success mx-3"
+              to="/"
+              style={{ textDecoration: "none" }}
+            >
+              <span>
+                <i className="fa fa-home mx-1"></i>
+              </span>
+              HOME
+            </Link>
+            <Link
+              to="/about"
+              className="mx-3"
+              style={{ textDecoration: "none", color: "#9f1094" }}
+            >
+              <span>
+                <i className="fa fa-user mx-1"></i>
+              </span>
+              ABOUT
+            </Link>
+            <Link
+              to="/class"
+              className="text-danger mx-3"
+              style={{ textDecoration: "none" }}
+            >
+              <span>
+                <i className="fa fa-file-text-o mx-1"></i>
+              </span>
+              CLASSES
+            </Link>
+            <Link
+              to="/blog"
+              className="text-info mx-3"
+              style={{ textDecoration: "none" }}
+            >
+              <span>
+                <i className="fa fa-pencil mx-1"></i>
+              </span>
+              BLOG
+            </Link>
+            <Link
+              to="/pages"
+              className="mx-3"
+              style={{ textDecoration: "none", color: "red" }}
+            >
+              <span>
+                <i className="fa fa-align-justify mx-1"></i>
+              </span>
+              PAGES
+            </Link>
+            <Link
+              className="text-primary mx-3"
+              to="/contact"
+              style={{ textDecoration: "none" }}
+            >
+              <span>
+                <i class="fa-solid fa-location-dot mx-1"></i>
+              </span>
+              CONTACT
+            </Link>
+            <Link to="" className="mx-3" style={{ color: "#9f1094" }}>
+              <i className="fa fa-search"></i>
+            </Link>
+            <Link
+              to=""
+              className="text-secondary mx-3"
+              onClick={handleTrashIconClick}
+            >
+              <i className="fa fa-trash-o"></i>
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      {showSidebar && (
+        <AddCart show={showSidebar} handleClose={handleCloseSidebar} />
+      )}
+    </>
   );
 };
