e01893df143d473027fc30871057cf5cd1ecc772
MMureed-Hussain <malikmureedh@gmail.com>
2023-09-21 13:43:51 +0500

    First commit

--- src/components/Navbars.jsx
+++ src/components/Navbars.jsx
@@ -2,13 +2,30 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
+// import logoImage from '/images/logo192.png'
+import logoImage from "../images/logo192.png";
 
 export const Navbars = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav" style={{justifyContent: "center"}}>
+      <Navbar.Collapse
+        id="basic-navbar-nav"
+        style={{ justifyContent: "center" }}
+      >
         <Nav className="mr-auto" style={{ fontWeight: "bold" }}>
+          <Link className="mx-3" to="/" style={{ textDecoration: "none" }}>
+            <img
+              src={logoImage}
+              alt="Logo"
+              style={{ maxWidth: "100px", maxHeight: "30px" }} // Adjust image dimensions as needed
+            />
+            <span style={{ color: "green" }}>K</span>
+            <span style={{ color: "blue" }}>i</span>
+            <span style={{ color: "red" }}>tt</span>
+            <span style={{ color: "blue" }}>o</span>
+            <span style={{ color: "red" }}>n</span>
+          </Link>
           <Link
             className="text-success mx-3"
             to="/"
